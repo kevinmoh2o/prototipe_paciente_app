71686e4de45f39a4403298b487d1bc3c4321dc4d
kevinmoh2o <kevinmohu@gmail.com>
2025-04-19 01:07:33 -0500

    add free tier first version

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1285920450"
+    serviceWorkerVersion: "2160856330"
   }
 });
